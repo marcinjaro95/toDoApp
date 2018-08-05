0d06a60822be882306222b9de6128d0a11564e3f
marcinjaro95 <33868361+marcinjaro95@users.noreply.github.com>
2018-08-05 22:12:18 +0200

    preparing items checked to show

--- storage.js
+++ storage.js
@@ -4,11 +4,14 @@ class Storage {
         let array = [];
         let items = document.querySelectorAll('.item');
         for (let i = 0; i < items.length; i++) {
-            if(!items[i].hasAttribute('style')) {
-                array.push(items[i].textContent.split(/.$/)[0].split(','));
+            if(!items[i].hasAttribute('style') && !items[i].classList.contains('checked')) {
+                array.push(items[i].textContent.split(/.$/)[0].split(',')+'[R]');
+            }
+            if(items[i].classList.contains('checked') && !items[i].hasAttribute('style')) {
+                array.push(items[i].textContent.split(/.$/)[0].split(',')+'[C]');
             }
         }
-        return array.join(',');
+        return array;
     }
 
     setItemInLocalStorage() {
@@ -16,24 +19,30 @@ class Storage {
     };
 
     prepareItemsToShow(item) {
-        let li = document.createElement('li');
-        let textNode = document.createTextNode(item);
-        let span = document.createElement('SPAN');
-        let text = document.createTextNode('\u00D7');
+        if(item) {
+            let li = document.createElement('li');
+            let textNode = document.createTextNode(item.replace(/\[R\]|\[C\]|^.|.$/g, ''));
+            let span = document.createElement('SPAN');
+            let text = document.createTextNode('\u00D7');
 
-        li.classList.add('item');
-        li.appendChild(textNode);
-        document.querySelector('.list').appendChild(li);
-        span.className = 'close';
-        span.appendChild(text);
-        li.appendChild(span);
+            li.classList.add('item');
+            if(item.match("[C]")) {
+                li.classList.add('checked');
+            }
+            li.appendChild(textNode);
+            console.log(textNode);
+            document.querySelector('.list').appendChild(li);
+            span.className = 'close';
+            span.appendChild(text);
+            li.appendChild(span);
+            let close = document.querySelectorAll('.close');
 
-        let close = document.querySelectorAll('.close');
+            close.forEach(elem => elem.onclick = function () {
+                let div = this.parentElement;
+                div.style.display = 'none';
+            });
+        }
 
-        close.forEach(elem => elem.onclick = function () {
-            let div = this.parentElement;
-            div.style.display = 'none';
-        });
     }
 
     getItemFromLocalStorage() {
